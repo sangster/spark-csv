deecd452c10a3b75433704d600ff03cac85e325c
Jon Sangster <jon@ertt.ca>
2018-08-14 21:15:00 -0700

    Basic STDOUT report generation

--- exercise-1/lib/report_generator.rb
+++ exercise-1/lib/report_generator.rb
@@ -3,14 +3,26 @@ class ReportGenerator
   QA_COLUMNS = %i[how_did_you_hear_about_us what_is_your_budget
                   what_is_your_favourite_color].freeze
 
-  TEMPLATE = <<-REPORT.gsub(/^\s+/, '').freeze
-    VALID CONTACTS
+  TEMPLATE = <<-TEMPLATE.gsub(/^ {4}/, '').freeze
+    Report
+    ============================================================================
+         Total Contacts: %<total_size>d
+     Duplicate Contacts: %<duplicate_size>d
+    Incomplete Contacts: %<incomplete_size>d
+
+    Valid Contacts
     ============================================================================
     %<valid>s
-  REPORT
+
+    Invalid Contacts
+    ============================================================================
+    %<invalid>s
+  TEMPLATE
 
   attr_reader :contacts
 
+  # @param contacts [Array<hash>] the list of contacts to report on
+  # @param output [IO] where to write the report to
   def initialize(contacts, output = $stderr)
     @contacts = contacts
     @output = output
@@ -19,7 +31,11 @@ class ReportGenerator
   def to_s
     format(
       TEMPLATE,
-      valid: merge(complete_contacts).map(&method(:render_contact)).join("\n\n")
+      total_size: contacts.size,
+      duplicate_size: duplicate_size,
+      incomplete_size: incomplete_contacts.size,
+      valid: merge(complete_contacts).map(&method(:contact_to_s)).join("\n\n"),
+      invalid: invalid_report
     )
   end
 
@@ -31,11 +47,15 @@ class ReportGenerator
     end
   end
 
+  def incomplete_contacts
+    contacts - complete_contacts
+  end
+
   # Merges a list of contacts such that entries with the same email are merged
   # into a single element, with values from more recent entries taking
   # precidence.
   #
-  # @param [Array<Hash>] the list of contacts
+  # @param contacts_with_dupes [Array<Hash>] the list of contacts
   # @return [Array<Hash>] the same list of contacts, with duplicate entries
   #   merged into one
   def merge(contacts_with_dupes)
@@ -60,7 +80,8 @@ class ReportGenerator
     end
   end
 
-  def render_contact(contact)
+  # @return [String] A nicely-formated representation of the given contact
+  def contact_to_s(contact)
     key_len = contact.keys.map { |k| k.to_s.size }.max
 
     contact.map do |key, value|
@@ -70,5 +91,30 @@ class ReportGenerator
         value: value.to_s.empty? ? '<N/A>' : value
       )
     end.join("\n")
+  end
+
+  def duplicate_size
+    group_by_email(contacts).select { |_, c| c.size > 1 }.size
+  end
+
+  def invalid_report
+    incomplete = incomplete_contacts
+
+    errors =
+      contacts.each_with_index.map do |contact, index|
+        next unless incomplete.include?(contact)
+
+        missing =
+          (contact.keys - QA_COLUMNS).select { |col| contact[col].to_s.empty? }
+
+        format(
+          'Row %<row>d: Blank column%<plural>s: %<cols>s',
+          row: index + 2, # 2 accounts for header + 0-index
+          cols: missing.map(&:to_s).join(', '),
+          plural: missing.size == 1 ? '' : 's'
+        )
+      end
+
+    errors.compact.join("\n")
   end
 end
