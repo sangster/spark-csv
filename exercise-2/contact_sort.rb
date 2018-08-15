a38e739710b6da3302d6cae9a4112a560dfda52c
Jon Sangster <jon@ertt.ca>
2018-08-14 21:46:04 -0700

    Improve ruby readability

--- exercise-2/contact_sort.rb
+++ exercise-2/contact_sort.rb
@@ -1,32 +1,28 @@
 #!/usr/bin/env ruby
-
-# run contact_sort.rb > results.html
-
 require 'csv'
 
-groups = {}
-rows = CSV.read('./contacts.csv', headers: true)
+INFILE = ARGV.size == 0 ? './contacts.csv' : ARGV[0]
 
-rows.each do |row|
-  data = {
+require 'byebug'
+
+groups = Hash.new { |hash, key| hash[key] = [] }
+
+CSV.read(INFILE, headers: true).each do |row|
+  groups[row['Group']] << {
     name: "#{row['First name']} #{row['Last name']}",
     email: row['Email address'],
     phone: row['Telephone']
   }
-
-  if groups[row['Group']]
-    groups[row['Group']] << data
-  else
-    groups[row['Group']] = [data]
-  end
 end
 
-groups.sort_by { |g, _| g }.each do |group, contacts|
+groups.sort.to_h.each do |group, contacts|
   puts "<h2>#{group}</h2>"
   puts '<table><tr><th>Name</th><th>Email</th><th>Phone</th></tr>'
+
   contacts.sort_by { |c| c[:name] }.each do |contact|
     puts "<tr><td>#{contact[:name]}</td><td>#{contact[:email]}</td>" \
          "<td>#{contact[:phone]}</td></tr>"
   end
+
   puts '</table>'
 end
