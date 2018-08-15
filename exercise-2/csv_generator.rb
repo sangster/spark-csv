696a593ad4e12841aeae54018650e1dead267e29
Jon Sangster <jon@ertt.ca>
2018-08-14 21:35:09 -0700

    Extract magic numbers

--- exercise-2/csv_generator.rb
+++ exercise-2/csv_generator.rb
@@ -3,14 +3,21 @@
 require 'faker'
 require 'csv'
 
+OUTFILE = ARGV.size == 0 ? './contacts.csv' : ARGV[0]
+ROW_SIZE = ARGV.size < 2 ? 10_000 : ARGV[1]
 GROUPS = %w[Group1 Group2 Group3 Group4].freeze
+HEADERS = ['First name', 'Last name', 'Email address', 'Telephone',
+           'Group'].freeze
 
-CSV.open('./contacts.csv', 'w+') do |csv|
-  csv << ['First name', 'Last name', 'Email address', 'Telephone', 'Group']
 
-  10_000.times do
+CSV.open(OUTFILE, 'w+') do |csv|
+  csv << HEADERS
+
+  ROW_SIZE.times do
+    name = Faker::Name.first_name
+
     csv << [
-      name = Faker::Name.first_name,
+      name,
       Faker::Name.last_name,
       Faker::Internet.email(name),
       Faker::PhoneNumber.cell_phone,
